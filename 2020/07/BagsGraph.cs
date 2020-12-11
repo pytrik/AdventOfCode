35ce4c49e59f79e6eb2d563a475040121f568028
Pytrik Elzinga <pytrik.elzinga@gmail.com>
2020-12-11 19:05:26 +0100

    Rename files to make more sense after refactor

--- 2020/07/BagsGraph.cs
+++ 2020/07/BagsGraph.cs
@@ -1,32 +1,9 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AOC.Y2020
 {
-    public class Bag
-    {
-        internal Bag(string name)
-        {
-            this.Name = name;
-        }
-
-        public string Name { get; protected set; }
-        public Dictionary<Bag, int> Children { get; } = new();
-        public HashSet<Bag> Parents { get; } = new();
-
-        public IEnumerable<Bag> GetAncestors()
-        {
-            var ancestors = this.Parents
-                .Select(p => p.GetAncestors())
-                .Aggregate(new List<Bag>(), (result, part) => result.Concat(part).ToList());
-            return this.Parents.Union(ancestors).Distinct();
-        }
-
-        public override string ToString() => $"{this.Name} bag";
-    }
-
     public class BagsGraph
     {
         private Dictionary<string, Bag> cache = new();
